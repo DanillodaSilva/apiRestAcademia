dd58c40220aab094bed8425e53d734b4d6d328bd
DanillodaSilva <dssiqueira2021@gmail.com>
2026-09-03 09:48:59 -0300

    Iniciando projeto

---
+++ academiaProjeto/src/main/java/br/com/daSIlva/academiaProjeto/AcademiaProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.daSIlva.academiaProjeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaProjetoApplication.class, args);
+	}
+
+}
