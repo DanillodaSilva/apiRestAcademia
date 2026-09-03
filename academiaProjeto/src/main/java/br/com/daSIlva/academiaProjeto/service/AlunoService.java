c626284d8ea8ae2f99042cc6def53fb1549763aa
DanillodaSilva <dssiqueira2021@gmail.com>
2026-09-03 10:35:30 -0300

    feat: Criei a classe service do aluno

---
+++ academiaProjeto/src/main/java/br/com/daSIlva/academiaProjeto/service/AlunoService.java
@@ -0,0 +1,17 @@
+package br.com.daSIlva.academiaProjeto.service;
+
+import br.com.daSIlva.academiaProjeto.database.repository.AlunoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AlunoService {
+
+    private final AlunoRepository alunoRepository;
+
+//    CADASTRAR
+//    LISTAR
+//    BUSCARPORID
+//    REMOVER
+}
